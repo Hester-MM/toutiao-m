3ba60259d1556ed223df50784234bea770148947
Hester <1595455407@qq.com>
2022-10-10 17:08:43 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'amfe-flexible'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
